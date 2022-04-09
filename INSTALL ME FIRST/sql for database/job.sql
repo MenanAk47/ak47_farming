7f4f6206c85666075038263b4f8d88b73c451221
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2022-04-09 19:00:07 +0600

    Update job.sql

--- INSTALL ME FIRST/sql for database/job.sql
+++ INSTALL ME FIRST/sql for database/job.sql
@@ -5,5 +5,5 @@ INSERT INTO `jobs` (name, label) VALUES
 
 DELETE FROM `job_grades` WHERE `job_name` = 'farmer';
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('farmer',0,'farmer','Farmer',20,'{}','{}'),
+	('farmer',0,'farmer','Farmer',20,'{}','{}')
 ;
